161c654dc00338ff343d70a94a6b249cf8264a51
Olamide-Bello <belloolamideakinniyi@gmail.com>
2023-10-10 20:09:52 +0100

    Fixed errors in videos.tsx

--- pages/videos.tsx
+++ pages/videos.tsx
@@ -46,166 +46,129 @@ function Videos() {
   };
 
 
-/*   const src =
-    'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4'
-
-  const details = [
-    {
-      src: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-      title: 'How to Create Facebook Ad Listing',
-      date: '  SEPTEMBER 22, 2023',
-    },
-    {
-      src: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-      title: 'How to Create Facebook Ad Listing',
-      date: '  SEPTEMBER 22, 2023',
-    },
-    {
-      src: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-      title: 'How to Create Facebook Ad Listing',
-      date: '  SEPTEMBER 22, 2023',
-    },
-    {
-      src: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-      title: 'How to Create Facebook Ad Listing',
-      date: '  SEPTEMBER 22, 2023',
-    },
-    {
-      src: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-      title: 'How to Create Facebook Ad Listing',
-      date: '  SEPTEMBER 22, 2023',
-    },
-    {
-      src: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-      title: 'How to Create Facebook Ad Listing',
-      date: '  SEPTEMBER 22, 2023',
-    },
-    {
-      src: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-      title: 'How to Create Facebook Ad Listing',
-      date: '  SEPTEMBER 22, 2023',
-    },
-  ] */
+  /*   const src =
+      'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4'
+  
+    const details = [
+      {
+        src: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+        title: 'How to Create Facebook Ad Listing',
+        date: '  SEPTEMBER 22, 2023',
+      },
+      {
+        src: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+        title: 'How to Create Facebook Ad Listing',
+        date: '  SEPTEMBER 22, 2023',
+      },
+      {
+        src: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+        title: 'How to Create Facebook Ad Listing',
+        date: '  SEPTEMBER 22, 2023',
+      },
+      {
+        src: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+        title: 'How to Create Facebook Ad Listing',
+        date: '  SEPTEMBER 22, 2023',
+      },
+      {
+        src: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+        title: 'How to Create Facebook Ad Listing',
+        date: '  SEPTEMBER 22, 2023',
+      },
+      {
+        src: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+        title: 'How to Create Facebook Ad Listing',
+        date: '  SEPTEMBER 22, 2023',
+      },
+      {
+        src: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+        title: 'How to Create Facebook Ad Listing',
+        date: '  SEPTEMBER 22, 2023',
+      },
+    ] */
   return (
- 
 
-  <div>
-      <div className="w-full h-screen overflow-y-hidden">
-        <div className="w-full h-full mb-8 flex flex-col   justify-between">
-          {/* <div className="w-full px-[4em] py-9 flex items-center justify-between">
-            <Image src={Logo2} alt="" className="w-[100px] " />
-            <button className="px-3 flex items-center justify-center gap-2"></button>
-          </div> */}
-          <Navbar />
-          <div className="w-full px-[6em] py-5 flex items-start       justify-between lg:flex-row md:flex-col sm:flex-col">
-            <div className="w-auto flex flex-col items-start justify-start">
-              <h1 className="text-black-600   lg:text-[2em]  sm:text-sm font-bold mb-3 ">
-                Hello, John Mark
-              </h1>
-              <p className="text-white-400 text-[12px] color-black-600 ">
-                Here are your recorded videos
-              </p>
-            </div>
-            <div className="w-full max-w-[250px] bg-white-300 px-4 flex items-center justify-start border rounded-lg">
-              <FiSearch size={15} color="#ccc" />
-              <input
-                type="text"
-                className="w-full py-3 bg-transparent outline-none border-none px-3 text-[10px] text-white-400 font-ppReg"
-                placeholder="Search for a video"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-        <div
-            style={{
-              borderTop: '1px solid lightgray',
-              paddingTop: '50px',
-            }}
-            className="  w-full px-[6em] min-h-[15em] mt-9 flex   justify-start flex-col gap-4 mb-5 overflow-y-scroll"
-          > 
-            <p className="text-dark-200 font-ppReg text-[12px] ">
-              Recent files </p>
-    <div className="w-full h-screen overflow-y-hidden">
-      <div className="w-full h-full mb-8 flex flex-col justify-between">
-        <Navbar />
-        <div className="w-full px-[6em] py-5 flex items-start justify-between lg:flex-row md:flex-col sm:flex-col">
-          <div className="w-auto flex flex-col items-start justify-start">
-            <h1 className="text-black-600 lg:text-[2em] sm:text-sm font-bold mb-3">
-              Hello, John Mark
-            </h1>
-            <p className="text-white-400 text-[12px] color-black-600 ">
-              Here are your recorded videos
- 
-            </p>
-          </div>
-          <div className="w-full max-w-[250px] bg-white-300 px-4 flex items-center justify-start border rounded-lg">
-            <FiSearch size={15} color="#ccc" />
-            <input
-              type="text"
-              className="w-full py-3 bg-transparent outline-none border-none px-3 text-[10px] text-white-400 font-ppReg"
-              placeholder="Search for a video"
-            />
-          </div>
-        </div>
-        <br />
-        <div className="w-full px-[6em] min-h-[15em] mt-9 flex justify-start flex-col gap-4 mb-5 overflow-y-scroll">
-          <p className="text-dark-200 font-ppReg text-[12px] ">Recent files</p>
-          <div className="w-full flex items-center justify-start flex-wrap gap-7">
-            {videos.map((item, index) => (
-                <Link key={index} href={`/videos/${item?.id}`} passHref>
-              <div
-                key={index}
-                className="w-full p-4 max-w-[500px] max-h-[250px] h-full border-solid border-white-300 border-[2px] rounded-[10px] "
-              >
-                <video
-                  controls
-                  className="w-full h-[150px] rounded-[10px] bg-white-300 object-cover"
-                >
-                  <source src={item.src} type="video/mp4" />
-                </video>
-                <div className="flex justify-between mt-3">
-                  <p className="text-dark-100 font-ppSB text-[15px] mt-1">
-                    {item.username}
-                  </p>
-                  <div className="flex">
-                    <Image
-                      src="/assets/video-repo/link.png"
-                      alt="stuff"
-                      width={20}
-                      height={20}
-                    />
-                    <Image
-                      src="/assets/video-repo/more.png"
-                      alt="stuff"
-                      width={20}
-                      height={20}
-                    />
-                  </div>
-                </div>
-                <p className="text-white-400 opacity-[.5] font-ppReg text-[11px]">
-                  {item?.created_date}
+
+    <div>
+        <div className="w-full h-screen overflow-y-hidden">
+          <div className="w-full h-full mb-8 flex flex-col justify-between">
+            <Navbar />
+            <div className="w-full px-[6em] py-5 flex items-start justify-between lg:flex-row md:flex-col sm:flex-col">
+              <div className="w-auto flex flex-col items-start justify-start">
+                <h1 className="text-black-600 lg:text-[2em] sm:text-sm font-bold mb-3">
+                  Hello, John Mark
+                </h1>
+                <p className="text-white-400 text-[12px] color-black-600 ">
+                  Here are your recorded videos
+
                 </p>
               </div>
-             
-          </Link>
-            ))}
+              <div className="w-full max-w-[250px] bg-white-300 px-4 flex items-center justify-start border rounded-lg">
+                <FiSearch size={15} color="#ccc" />
+                <input
+                  type="text"
+                  className="w-full py-3 bg-transparent outline-none border-none px-3 text-[10px] text-white-400 font-ppReg"
+                  placeholder="Search for a video"
+                />
+              </div>
+            </div>
+            <br />
+            <div className="w-full px-[6em] min-h-[15em] mt-9 flex justify-start flex-col gap-4 mb-5 overflow-y-scroll">
+              <p className="text-dark-200 font-ppReg text-[12px] ">Recent files</p>
+              <div className="w-full flex items-center justify-start flex-wrap gap-7">
+                {videos.map((item, index) => (
+                  <Link key={index} href={`/videos/${item?.id}`} passHref>
+                    <div
+                      key={index}
+                      className="w-full p-4 max-w-[500px] max-h-[250px] h-full border-solid border-white-300 border-[2px] rounded-[10px] "
+                    >
+                      <video
+                        controls
+                        className="w-full h-[150px] rounded-[10px] bg-white-300 object-cover"
+                      >
+                        <source src={item.src} type="video/mp4" />
+                      </video>
+                      <div className="flex justify-between mt-3">
+                        <p className="text-dark-100 font-ppSB text-[15px] mt-1">
+                          {item.username}
+                        </p>
+                        <div className="flex">
+                          <Image
+                            src="/assets/video-repo/link.png"
+                            alt="stuff"
+                            width={20}
+                            height={20}
+                          />
+                          <Image
+                            src="/assets/video-repo/more.png"
+                            alt="stuff"
+                            width={20}
+                            height={20}
+                          />
+                        </div>
+                      </div>
+                      <p className="text-white-400 opacity-[.5] font-ppReg text-[11px]">
+                        {item?.created_date}
+                      </p>
+                    </div>
+
+                  </Link>
+                ))}
+              </div>
+            </div>
           </div>
+          <ToastContainer
+            position="top-center" // Position the toast container at the bottom-center
+            autoClose={1500} // Close after 3 seconds (adjust as needed)
+            style={{
+              width: 'fit-content', // Adjust the width as needed
+              textAlign: 'center', // Center-align the container's content
+            }}
+          />
         </div>
-      </div>
-      <ToastContainer 
-          position="top-center" // Position the toast container at the bottom-center
-          autoClose={1500} // Close after 3 seconds (adjust as needed)
-          style={{
-         width: 'fit-content', // Adjust the width as needed
-         textAlign: 'center', // Center-align the container's content
-         }}
-       />
-    </div>
-    </div>
     </div>
   );
- 
+
 }
-        
+
 export default Videos

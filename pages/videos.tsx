f6db0f1cd830575c5edd134879371b0bc54381ca
areaboxer <jamicojerahmeel@gmail.com>
2023-10-10 20:09:19 +0100

    make responsive

--- pages/videos.tsx
+++ pages/videos.tsx
@@ -49,86 +49,67 @@ function Videos() {
   ]
   return (
     <div>
-      <div className="w-full h-screen overflow-y-hidden">
-        <div className="w-full h-full mb-8 flex flex-col   justify-between">
-          {/* <div className="w-full px-[4em] py-9 flex items-center justify-between">
-            <Image src={Logo2} alt="" className="w-[100px] " />
-            <button className="px-3 flex items-center justify-center gap-2"></button>
-          </div> */}
+      <div className="w-full min-h-screen overflow-y-hidden">
+        <div className="w-full min-h-full mb-8 flex flex-col justify-between">
           <Navbar />
-          <div className="w-full px-[6em] py-5 flex items-start justify-between lg:flex-row md:flex-col sm:flex-col">
-            <div className="w-auto flex flex-col items-start justify-start">
-              <h1 className="text-black-600   lg:text-[2em]  sm:text-sm font-bold mb-3 ">
+          <div className="w-full px-4 lg:px-12 py-5 flex flex-col lg:flex-row md:flex-col sm:flex-col items-center lg:items-start justify-between">
+            <div className="w-full lg:w-auto flex flex-col items-start justify-start">
+              <h1 className="text-2xl lg:text-3xl sm:text-sm font-bold mb-3">
                 Hello, John Mark
               </h1>
-              <p className="text-white-400 text-[12px] color-black-600 ">
+              <p className="text-gray-400 text-xs sm:text-sm">
                 Here are your recorded videos
               </p>
             </div>
-            <div className="w-full max-w-[250px] bg-white-300 px-4 flex items-center justify-start border rounded-lg">
+            <div className="w-full max-w-xs lg:max-w-[250px] bg-white px-2 lg:px-4 flex items-center justify-center border rounded-lg mt-3 lg:mt-0">
               <FiSearch size={15} color="#ccc" />
               <input
                 type="text"
-                className="w-full py-3 bg-transparent outline-none border-none px-3 text-[10px] text-white-400 font-ppReg"
+                className="w-full py-2 bg-transparent outline-none border-none px-2 text-sm text-gray-400"
                 placeholder="Search for a video"
               />
             </div>
           </div>
 
-          <div
-            style={{
-              borderTop: '1px solid lightgray',
-              paddingTop: '50px',
-            }}
-            className="  w-full px-[6em] min-h-[15em] mt-9 flex   justify-start flex-col gap-4 mb-5 overflow-y-scroll"
-          >
-            <p className="text-dark-200 font-ppReg text-[12px] ">
+          <div className="w-full px-4 lg:px-12 mt-9 flex flex-col gap-4 overflow-y-scroll">
+            <p className="text-gray-600 font-medium text-sm lg:text-base">
               Recent files
             </p>
-            <div className="w-full flex items-center   justify-start flex-wrap gap-7">
-              {/* <Spinner color="#000" /> */}
+            <div className="w-full flex items-center justify-start flex-wrap gap-4 lg:gap-7">
               {details.map((item, index) => (
-                <div className="w-full p-4 max-w-[500px]  max-h-[250px] h-full   border-solid border-white-300 border-[2px]   rounded-[10px] ">
+                <div
+                  key={index}
+                  className="w-full lg:w-1/2 p-2 md:p-4 lg:p-2 xl:p-4 max-w-[500px] max-h-[250px] lg:max-h-[300px] border border-gray-200 rounded-md"
+                >
                   <video
                     controls
-                    className="w-full h-[150px] rounded-[10px] bg-white-300 object-cover"
+                    className="w-full h-[150px] lg:h-[200px] rounded-md bg-gray-300 object-cover"
                   >
                     <source src={item.src} type="video/mp4" />
                   </video>
-                  <div className="flex justify-between mt-3">
-                    <p className="text-dark-100 font-ppSB text-[15px] mt-1">
-                      {' '}
+                  <div className="flex justify-between mt-2">
+                    <p className="text-black-700 font-medium text-sm lg:text-base">
                       {item.title}
                     </p>
-                    <div className="flex  ">
+                    <div className="flex">
                       <img
-                        className="object-contain"
+                        className="w-4 h-4 object-contain mr-2"
                         src="/assets/video-repo/link.png"
-                        alt="stuff"
+                        alt="Link"
                       />
                       <img
-                        className="object-contain"
+                        className="w-4 h-4 object-contain"
                         src="/assets/video-repo/more.png"
-                        alt="stuff"
+                        alt="More"
                       />
                     </div>
                   </div>
-
-                  <p className="text-white-400 opacity-[.5] font-ppReg text-[11px]">
+                  <p className="text-gray-400 opacity-50 text-xs lg:text-sm">
                     {item.date}
                   </p>
                 </div>
               ))}
-
-              {/* <p className="text-white-400 text-[13px]">recent stuff</p> */}
             </div>
-            <br />
-            {/* <p className="text-dark-200 font-ppReg text-[12px] ">
-            Files from last week.
-          </p>
-          <div className="w-full flex items-center justify-start flex-wrap">
-            <VideoCard />
-          </div> */}
           </div>
         </div>
       </div>

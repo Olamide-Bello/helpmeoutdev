c29e994869a254f6018f91082e6e97dbc68aaea4
thelouisgram <hadesanoye01@gmail.com>
2023-10-11 20:19:50 +0100

    images

--- components/LandingPage/Hero.tsx
+++ components/LandingPage/Hero.tsx
@@ -12,13 +12,14 @@ const Hero = () => {
             Show Them Don’t Just Tell
           </h1>
           <p className="text-black-100 text-[20px] text-center md:text-left font-Inter mb-[48px]">
-            Help your friends and loved ones by creating and sending videos  on
+            Help your friends and loved ones by creating and sending videos on
             how to get things done on a website.
           </p>
           <div className="w-full flex justify-center md:justify-start">
-            <a 
-            href='https://drive.google.com/file/d/1t2mgIO4mYdZwI2Ex41Sqz-iWuWUPKUnc/view?usp=drive_link'
-            className="px-[24px] py-[22px] bg-primary-600 rounded-[8px] flex items-center gap-[10px] text-pastel-bg text-[18px]">
+            <a
+              href="https://drive.google.com/file/d/1t2mgIO4mYdZwI2Ex41Sqz-iWuWUPKUnc/view?usp=drive_link"
+              className="px-[24px] py-[22px] bg-primary-600 rounded-[8px] flex items-center gap-[10px] text-pastel-bg text-[18px]"
+            >
               <p>Install HelpMeOut</p>
               <Image
                 src="/assets/home/arrow-right.svg"
@@ -30,14 +31,48 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      <div className="w-full h-auto flex flex-col md:flex-row gap-[20px]">
-        <Image
-          src="/assets/home/hero-img.png"
-          alt="hero-img"
-          width="1000"
-          height={1000}
-          className="w-full h-auto"
-        />
+      <div className="w-full h-auto flex flex-row gap-3 md:gap-[20px] md:px-[40px] relative">
+        <div className="hidden lg:flex">
+          <Image
+            src="/assets/home/grey-grid.svg"
+            alt=""
+            className="absolute right-[5px] z-[1] top-[-44px]"
+            width={250}
+            height={250}
+          />
+          <Image
+            src="/assets/home/blue-grid.svg"
+            alt=""
+            className="absolute left-[16px] z-[1] bottom-[-66px]"
+            width={250}
+            height={250}
+          />
+        </div>
+        <div className="flex flex-col gap-2 md:gap-0  justify-between z-[2]">
+          <Image
+            src="/assets/home/hero (2).png"
+            alt="Woman"
+            className="rounded-[8px]"
+            width={1000}
+            height={1000}
+          />
+          <Image
+            src="/assets/home/hero (3).png"
+            alt="man"
+            className="rounded-[8px]"
+            width={1000}
+            height={1000}
+          />
+        </div>
+        <div className="z-[2] ">
+          <Image
+            src="/assets/home/hero (1).png"
+            alt="woman-using-smartphone"
+            className="rounded-[8px]"
+            width={1000}
+            height={1000}
+          />
+        </div>
       </div>
     </div>
   )

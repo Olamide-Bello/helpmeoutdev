7d3cc14eb5c6a0f2ff9c7a0752d93f56fe721257
Olamide-Bello <belloolamideakinniyi@gmail.com>
2023-10-13 20:02:53 +0100

    commented out transcript

--- components/RecordingReadyPage/Transcript.tsx
+++ components/RecordingReadyPage/Transcript.tsx
@@ -97,11 +97,11 @@ const Transcript: React.FC<TranscriptProps> = ({ videoID, currentVideoTime, curr
           height="16"
         />
       </div>
-      
+
       <div className="w-full h-auto relative">
         <div className="font-Inter w-full h-[164px] border-[1px] rounded-[12px]  ss:border-none p-3 ss:h-[255px]   gap-4 relative ">
           <div className='p-2 overflow-y-scroll custom-scrollbar  gap-4 h-full pt-10 ' id='org-transcipt-container'>
-            
+
 
             {intervals.map((startTime, index) => {
               const endTime = startTime + intervalDuration;
@@ -110,24 +110,24 @@ const Transcript: React.FC<TranscriptProps> = ({ videoID, currentVideoTime, curr
               return (
                 <div key={index} className='flex'>
                   <h5 className="font-[400] w-1/12  font-Work-Sans text-[14px] xs:text-[16px] text-black  py-2 mr-3">
-                  {formatTime(startTime)}
+                    {formatTime(startTime)}
                   </h5>
                   {/* <p>{startTime} - {endTime}</p> */}
                   <div className="w-11/12 flex flex-wrap py-2">
-                  {wordsInInterval.map((item, wordIndex) => (
+                    {wordsInInterval.map((item, wordIndex) => (
                       //mapping with key 'wordIndex'
                       <div key={wordIndex} id="transcript-container" ref={transcriptContainerRef} className="custom-scrollbar  overflow-x-auto flex flex-wrap" >
-                        <p  id={`transcript-${item.start}`} className="mr-1 text-gray-400">
+                        <p id={`transcript-${item.start}`} className="mr-1 text-gray-400">
                           <strong>{item.punctuated_word}</strong>
                         </p>
                       </div>
-                  ))}
+                    ))}
                   </div>
                 </div>
               );
             })}
 
-            
+
           </div>
         </div>
       </div>

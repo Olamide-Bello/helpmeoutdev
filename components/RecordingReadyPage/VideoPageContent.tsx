01f93f5823f4fa9e8d7c90c64b9861fdbf9eace0
Mahendra <mahendradewangan195@gmail.com>
2023-10-16 20:24:16 +0530

    fix the gap in videoPageContent above videoInfo component

--- components/RecordingReadyPage/VideoPageContent.tsx
+++ components/RecordingReadyPage/VideoPageContent.tsx
@@ -9,7 +9,7 @@ const VideoPageContent: React.FC<VideoPageContentProps> = ({
 }) => {
   return (
     <div className=" font-Sora w-full h-auto md:h-[970px] ss:pt-[40px] flex md:justify-between 
-    flex-col md:flex-row items-start gap-[10px] ss:gap-[72px] my-[32px] xs:my-[64px]">
+    flex-col md:flex-row items-start gap-[12px] ss:gap-[72px] my-[32px] xs:my-[64px]">
       {/* Video information for tablet and Desktop view */}
       <VideoInfo displayModal={displayModal} videoID={videoID} setEmail={setEmail} email={email}/>
       {/* Video & information for mobile view */}

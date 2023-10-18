ef192be15c1a038f2eff3db36df534b180e91d67
amenyenudaniel <amenyenudaniel321@gmail.com>
2023-10-18 22:39:16 +0000

    UI

--- pages/editUsername/index.tsx
+++ pages/editUsername/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
 const EditUsername = () => {
-  const [newUsername, setNewUsername] = useState('')
+  const [newUsername, setNewUsername] = useState<string>('')
 
   const handleSubmit = () => {}
 

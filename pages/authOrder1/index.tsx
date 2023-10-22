d6f3406d72923bfe6a67743c5fa00ed161d77609
Ekpo Sampson <sampsonekpo6@gmail.com>
2023-10-22 21:07:39 +0100

    changed protected path

--- pages/authOrder1/index.tsx
+++ pages/authOrder1/index.tsx
@@ -3,20 +3,30 @@ import { useRouter } from 'next/router'
 import { GlobalContext } from '@/context/GlobalContext'
 
 const withAuth = (WrappedComponent: any) => {
-  return (props: any) => {
+  const ComponentWithAuth = (props: any) => {
     const router = useRouter()
     const { user, logged } = useContext(GlobalContext)
 
-    if (user === '' && !logged) {
+    if (user === '' && logged === false) {
       // Redirect to the signUp page
       if (typeof window !== 'undefined') {
-        router.replace('/signUp');
+        router.replace('/logIn')
       }
-      return null
+    return null
     }
 
     return <WrappedComponent {...props} />
   }
+
+  // Set the displayName for the HOC
+  ComponentWithAuth.displayName = `withAuth(${getDisplayName(WrappedComponent)})`
+
+  return ComponentWithAuth
+}
+
+// Helper function to get the display name of a component
+function getDisplayName(WrappedComponent: any) {
+  return WrappedComponent.displayName || WrappedComponent.name || 'Component'
 }
 
 export default withAuth
